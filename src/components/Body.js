ae5a6b8dec3845359d01a24436076429c6bf7ebb
William Baele <baelewilliam1999@gmail.com>
2023-11-16 18:08:24 +0100

    Building UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,8 +11,10 @@ const Body = () => {
             alt="profile pictur"
           />
         </div>
-        <div className="w-3/4 h-full bg-green-200">
-          <p>hi</p>
+        <div className="w-3/4 h-full bg-green-200 flex flex-col gap-4">
+          <h2 className="text-white text-2xl">Lorrem ipsum</h2>
+          <p className="text-[#2563eb] text-md">@loremipsumofficiel</p>
+          <p className="text-gray-200 text-md">@loremipsumofficiel</p>
         </div>
       </div>
     </div>
