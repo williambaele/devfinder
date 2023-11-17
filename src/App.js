f7acb7677c8ff1cbe75e219976140669df9d1767
William Baele <baelewilliam1999@gmail.com>
2023-11-17 12:09:42 +0100

    Building UI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  //DARK MODE
   const [darkMode, setDarkMode] = useState(false);
 
+  //SEARCH
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearchChange = (term) => {
+    setSearchTerm(term);
+  };
   return (
     <div className={`bg-[#131C30] h-screen ${darkMode ? "dark" : "bg-red-50"}`}>
       <div className="container mx-auto w-full md:w-1/2 px-4 md:px-0 py-6 md:py-10 space-y-6">
         <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-        <SearchBar />
+        <SearchBar onSearchChange={handleSearchChange} />
         <Body />
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 
-const SearchBar = () => {
+const SearchBar = ({ onSearchChange }) => {
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    onSearchChange(e.target.value);
+  };
+
   return (
     <div className="w-full bg-[#1F2A49] rounded-xl p-2 h-14 px-4 flex items-center">
       <div className="h-full flex items-center">
@@ -14,6 +21,7 @@ const SearchBar = () => {
       </div>
       <div className="h-full w-full  flex items-center">
         <input
+          onChange={handleSearchChange}
           placeholder="Search GitHub username"
           className="h-full w-full pl-2 text-white bg-transparent outline-none"
         />
